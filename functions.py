4c06ac249bf10aac5ede129f3a830b65f4be5c03
Bernardo Lopes <bernardolpz@gmail.com>
2025-05-29 16:35:33 -0300

    Refatorando o código

--- functions.py
+++ functions.py
@@ -1,11 +1,7 @@
-
-
-# Spades, Hearts, Diamonds, Clubs
-# AS = Ace of Spades
-# JS = Jack of Spades
-
-
-def deck(rules):
+def create_deck(rules):
+    # Spades, Hearts, Diamonds, Clubs
+    # AS = Ace of Spades
+    # JS = Jack of Spades
     deck_of_cards = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
     playable_deck = []
     if rules == 'Blackajck++':
@@ -15,9 +11,12 @@ def deck(rules):
         playable_deck += ['GC','SC'] #Adicionando a Golden Card e a Skull Card
     else:
         playable_deck += deck_of_cards * 4
-
     return playable_deck
 
 
 
-print(deck('Blackajck++'))
+if __name__ == "__main__":
+    rules = input("Modos de jogo:\n0 - Normal (Regras Padrão)\n1 - Blackajck++ (Regras Personalizada)\nInsira o modo de jogo: ")
+    mode = 'Blackajck++' if rules in ['1', 'Y', 'y'] else 'Normal'
+    deck = create_deck(mode)
+    print(f'Baralho de jogo: {deck}')
